0a5daa196350d88b81346e83f09da81fc4bc9ebf
Lukáš Boura <lukas.boura@gmail.com>
2019-09-23 12:52:12 +0200

    Add plurals support to generate command

--- src/commands/generateStrings.js
+++ src/commands/generateStrings.js
@@ -1,36 +1,67 @@
+const fs = require("fs");
+
 const ora = require("ora");
 
 const conf = require("../config");
 const xlsx = require("../xlsx");
 const { getPlatformStringsFromSheets } = require("../sheets");
 const { prepareStringValueForPlatform } = require("../strings");
-const { exportStrings } = require("../files");
+const files = require("../files");
 
 const spinner = ora();
 
 module.exports = async function generateStrings() {
-    spinner.start("Downloading xlsx file");
     try {
-        const xlsxBuffer = await xlsx.download(conf.xlsxUrl);
-
-        spinner.succeed();
-        spinner.start("Making localization files");
-
-        const sheets = xlsx.parse(xlsxBuffer);
-        const strings = getPlatformStringsFromSheets(sheets, (duplicates) =>
-            spinner.warn(duplicates)
-        ).map((string) => {
-            return {
-                ...string,
-                value: prepareStringValueForPlatform(
-                    string.value,
-                    conf.platform
-                )
-            };
-        });
-        exportStrings(strings, conf.developmentLanguage);
-        spinner.succeed();
+        const strings = await _downloadStrings();
+        _exportStrings(strings);
+        _exportPluralsStringsIfNeeded();
     } catch (error) {
         spinner.fail(error.message);
     }
 };
+
+async function _downloadStrings() {
+    spinner.start("Downloading xlsx file");
+
+    const xlsxBuffer = await xlsx.download(conf.xlsxUrl);
+    const sheets = xlsx.parse(xlsxBuffer);
+
+    const strings = getPlatformStringsFromSheets(sheets, (duplicates) =>
+        spinner.warn(duplicates)
+    ).map((string) => {
+        return {
+            ...string,
+            value: prepareStringValueForPlatform(string.value, conf.platform)
+        };
+    });
+
+    spinner.succeed();
+
+    return strings;
+}
+
+function _exportStrings(strings) {
+    spinner.start("Exporting strings file");
+    files.exportStrings(strings, conf.developmentLanguage);
+    spinner.succeed();
+}
+
+async function _exportPluralsStringsIfNeeded() {
+    spinner.start("Exporting plurals strings file");
+
+    if (!conf.hasPlurals()) return;
+
+    const pluralsPath = conf.getPluralsPath();
+    const pluralsFileName = conf.getPluralsFileName();
+    const pluralsFileContent = fs.readFileSync(pluralsPath, "utf8");
+
+    if (pluralsFileContent) {
+        await files.exportFile(
+            pluralsFileContent,
+            pluralsFileName,
+            conf.developmentLanguage
+        );
+    }
+
+    spinner.succeed();
+}
